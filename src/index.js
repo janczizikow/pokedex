698e003424e5567e48e55fe86722d3df406061a4
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-21 17:55:21 +0200

    Add redux and redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './store/reducer';
 import 'normalize.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  pokemons: null,
+  loading: false,
+  error: null
+};
+
+const reducer = (state = initialState, action ) => {
+  switch(action.type) {
+    default: return state;
+  }
+};
+
+export default reducer;
