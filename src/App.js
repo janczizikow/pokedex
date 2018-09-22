4c562926411e3377785e55cfdb8dc70f0b3b610c
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-22 20:32:48 +0200

    Preview the Card in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Header from './components/Header';
-import { Container } from './components/Grid';
+import { Container, Row, Col } from './components/Grid';
+import Card from './components/Card';
+import CardImg from './components/CardImg';
+import CardBody from './components/CardBody';
 import './styles/App.css';
 
-const App = () => (
+export const App = () => (
   <div className="App">
     <Header />
     <main className="App__content">
       <Container>
-        <p>content</p>
+        <Row>
+          <Col sm={6} md={4} lg={3}>
+            <Card>
+              <CardImg
+                src="http://www.serebii.net/pokemongo/pokemon/001.png"
+                alt="pokemon"
+              />
+              <CardBody num="#001" heading="Bulbasaur">
+                <p>Labels here</p>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </main>
   </div>
 );
 
-export default App;
+export default connect()(App);
