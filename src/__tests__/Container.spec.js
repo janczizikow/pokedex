885f2657da97a8f22d93488298e6f7373a3caa10
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-22 16:05:07 +0200

    Add <Container /> component

---
+++ src/__tests__/Container.spec.js
@@ -0,0 +1,33 @@
+import { Container } from '../components/Grid';
+
+describe('<Container />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Container />);
+  });
+
+  it('renders without crashing', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+
+  it('renders children', () => {
+    wrapper.setProps({ children: 'child' });
+    expect(wrapper.text()).toBe('child');
+  });
+
+  it('renders custom Tag', () => {
+    wrapper.setProps({ tag: 'section' });
+    expect(wrapper.type()).toBe('section');
+  });
+
+  it('renders additional classNames', () => {
+    wrapper.setProps({ className: 'my-class' });
+    expect(wrapper.find('.Container').hasClass('my-class')).toBeTruthy();
+  });
+
+  it('renders fluid class when given a prop', () => {
+    wrapper.setProps({ fluid: true });
+    expect(wrapper.hasClass('Container--fluid')).toBeTruthy();
+  });
+});
