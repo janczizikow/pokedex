b4d8d5b7c705942701138c02c8ed04d8d32eda4f
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-22 17:47:34 +0200

    Simplify App.spec.js

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,13 +1,8 @@
 import App from '../App';
 
 describe('<App />', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<App />);
-  });
-
   it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
     expect(wrapper).toHaveLength(1);
   });
 });
