101532dc6f798acc432f4344c498ea3812c4d26d
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-23 17:41:05 +0200

    Remove Redux devtools in production

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ import thunk from 'redux-thunk';
 import reducer from './store/reducer';
 import 'normalize.css';
 import './styles/index.css';
-import App from './App';
+import AppWithConnect from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 /* eslint-disable no-underscore-dangle */
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
 /* eslint-enable no-underscore-dangle */
 
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AppWithConnect />
   </Provider>,
   document.getElementById('root')
 );
