5003efeab3b0dbe2fc278b2978a08b70f4f3dfa6
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-23 13:54:52 +0200

    Improve Card specs

--- src/__tests__/Card.spec.js
+++ src/__tests__/Card.spec.js
@@ -10,4 +10,19 @@ describe('<Card />', () => {
   it('renders without crashing', () => {
     expect(wrapper).toHaveLength(1);
   });
+
+  it('renders children', () => {
+    wrapper.setProps({ children: 'child' });
+    expect(wrapper.text()).toBe('child');
+  });
+
+  it('renders additional classNames', () => {
+    wrapper.setProps({ className: 'my-class' });
+    expect(wrapper.hasClass('my-class')).toBeTruthy();
+  });
+
+  it('renders custom Tag', () => {
+    wrapper.setProps({ tag: 'a' });
+    expect(wrapper.type()).toBe('a');
+  });
 });
