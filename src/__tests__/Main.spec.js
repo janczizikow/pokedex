19371c4342016832124c748468a62b75681f6164
Jan Czizikow <jan.czizikow@gmail.com>
2018-09-24 22:46:39 +0200

    Need to figure out how to test Modal and the Main contianer

---
+++ src/__tests__/Main.spec.js
@@ -0,0 +1,11 @@
+// import { Main } from '../containers/Main';
+
+describe('<Main />', () => {
+  // let wrapper;
+
+  // beforeEach(() => {
+  //   wrapper = shallow(<Main />);
+  // });
+
+  it('renders without crashing', null);
+});
